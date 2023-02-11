75d807d0ad14c72bb2b0c20e67c48f0657e2dd7a
mutyalasrinivas <srinivasmutyala54@gmail.com>
2023-02-12 00:33:12 +0530

    middlewares & bodyparser added

--- express/app.js
+++ express/app.js
@@ -1,16 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 
 const app = express();
 
-app.use((req,res,next)=>{
-  console.log("srinivas")
-    next()
+app.use(bodyParser.urlencoded({extended:true}))
 
+app.use('/add-products',(req,res,next)=>{
+    res.send('<form action="/products" method="POST"><input type="text" name="title" ><input type="number" name="size"><button type="submit">submit</button></form>')
 })
-app.use((req,res,next)=> {
-    res.send('<h1>Welcome to Sharpener World</h1>');
+
+app.post('/products',(req,res,next)=> {
+    console.log(req.body);
+    console.log("products page")
+    res.redirect('/');
     
 })
+
+app.use('/',(req,res,next)=>{
+    res.send('<h1>from express!</h1>')
+  
+  })
 
 
 
