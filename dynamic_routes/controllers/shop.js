f584ea528082a77d1a3625e8120817c0ef4a4a08
mutyalasrinivas <srinivasmutyala54@gmail.com>
2023-02-16 13:55:51 +0530

    update cart features

--- dynamic_routes/controllers/shop.js
+++ dynamic_routes/controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -11,10 +12,10 @@ exports.getProducts = (req, res, next) => {
 };
 exports.getProduct=(req,res,next)=>{
   const prodId = req.params.productId;
-  Product.findById(prodId,Product=>{
-    console.log(Product);
+  Product.findById(prodId,product=>{
+    res.render('shop/product-detail',{product:product,pageTitle:product.title,path:'/products'});
   })
-  res.redirect('/');
+   
 }
 
 exports.getIndex = (req, res, next) => {
@@ -33,6 +34,12 @@ exports.getCart = (req, res, next) => {
     pageTitle: 'Your Cart'
   });
 };
+
+exports.postCart = (req,res,next)=>{
+  const prodId=req.body.productId;
+  console.log(prodId);
+  res.redirect('/cart');
+}
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
---
+++ dynamic_routes/models/cart.js
@@ -0,0 +1,35 @@
+const fs=require('fs');
+const path = require('path');
+
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'cart.json'
+);
+
+
+module.exports = class Cart{
+ static addProducts(id){
+     fs.readFile(p,(err, fileContent)=>{
+        let cart={products:[],totalPrice:0};
+        if(!err) {
+            cart=JSON.parse(fileContent);
+        }
+        const existingProduct = cart.products.find(prod=>prod.id===id);
+        let updatedProduct;
+        if(exisitingProduct){
+            updatedProduct={ ...existingProduct};
+            updatedProduct.qty=updatedProduct.qty+1;
+            cart.products=[...cart.products];
+            cart.products[exisitingProductIndex]=updatedProduct;
+        }else{
+            updatedProduct={id:id,qty:1};
+            cart.products=[...cart.products,updatedProduct];
+        }
+        cart.totalPrice=cart.totalPrice+ +productPrice;
+        fs.writeFile(p,JSON.stringify(cart),(err)=>{
+            console.log(err);
+        })
+     })
+ }
+}
