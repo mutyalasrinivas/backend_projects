1adde48fac907436036bf424454dd35e9eac1af2
mutyalasrinivas <srinivasmutyala54@gmail.com>
2023-02-22 21:28:52 +0530

    all features updated with sequelize

--- shop_app_db/util/database.js
+++ shop_app_db/util/database.js
@@ -1,7 +1,8 @@
  const Sequelize = require('sequelize');
- const sequelize = new Sequelize('node-complete','root','Srinivas99',{
+ const sequelize = new Sequelize('node-complete','root','sri9912',{
      dialect : 'mysql',
      host : 'localhost'
+     
  });
 
  module.exports = sequelize;
