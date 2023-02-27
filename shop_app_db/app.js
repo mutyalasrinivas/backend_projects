0ba450fb02d34e4b2fae8258752df98397bf9949
mutyalasrinivas <srinivasmutyala54@gmail.com>
2023-02-27 16:12:24 +0530

    Cart with many to many relationship

--- shop_app_db/app.js
+++ shop_app_db/app.js
@@ -16,6 +16,8 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const User = require('./models/user');
 const Product= require('./models/product');
+const Cart = require('./models/cart');
+const CartItem=require('./models/cart-item');
 
  
 
@@ -38,8 +40,13 @@ app.use(errorController.get404);
 
 Product.belongsTo(User,{constraints:true,onDelete:'CASCADE'});
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product,{through:CartItem});
+Product.belongsToMany(Cart,{through:CartItem});
 
 sequelize
+  // .sync({force:true})
   .sync()
   .then(result=>{
        return User.findByPk(1); 
@@ -51,6 +58,10 @@ sequelize
     return user;
   })
   .then(user=>{
+    return user.createCart();
+    
+  })
+  .then(cart=>{
     app.listen(3000);
   })
   .catch(err=> console.log(err))
