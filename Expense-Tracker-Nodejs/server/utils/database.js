1b3ec6425486094b6c05e725931b2a3a58c483d3
mutyalasrinivas <srinivasmutyala54@gmail.com>
2023-04-02 16:33:43 +0530

    deployment ready

--- Expense-Tracker-Nodejs/server/utils/database.js
+++ Expense-Tracker-Nodejs/server/utils/database.js
@@ -1,16 +1,13 @@
 const mysql = require('mysql2');
-const dotenv = require('dotenv');
+const dotenv = require('dotenv') 
 const Sequelize = require("sequelize");
 
-dotenv.config();
-const sequelize = new Sequelize('expense','root','sri9912',{
+dotenv.config({path:".env"});
+ 
+const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USERNAME,process.env.DB_PASSWORD,{
     dialect:"mysql",
-    host:"localhost"
+    host:process.env.DB_HOST 
 })
-// const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USERNAME,process.env.DB_PASSWORD,{
-//     dialect:"mysql",
-//     host:DB_HOST
-// })
 
 
 module.exports=sequelize;
